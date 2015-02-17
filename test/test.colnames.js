2fcc9b2d18d74b581147c48b9e0e095a973e8b5d
kgryte <kgryte@gmail.com>
2015-02-17 03:22:36 -0800

    [TESTS] row and col names.

---
+++ test/test.colnames.js
@@ -0,0 +1,79 @@
+/* global require, describe, it, beforeEach */
+'use strict';
+
+// MODULES //
+
+var // Expectation library:
+	chai = require( 'chai' ),
+
+	// Module to be tested:
+	DataFrame = require( './../lib' );
+
+
+// VARIABLES //
+
+var expect = chai.expect,
+	assert = chai.assert;
+
+
+// TESTS //
+
+describe( 'colnames', function tests() {
+
+	var data, df;
+
+	beforeEach( function beforeEach() {
+		data = [
+			[1,2,3],
+			[4,5,6]
+		];
+		df = new DataFrame( data );
+	});
+
+	it( 'should provide a method to set/get data frame column names', function test() {
+		expect( df.colnames ).to.be.a( 'function' );
+	});
+
+	it( 'should throw an error if not provided an array', function test() {
+		var values = [
+			'5',
+			5,
+			null,
+			undefined,
+			NaN,
+			function(){},
+			{}
+		];
+
+		for ( var i = 0; i < values.length; i++ ) {
+			expect( badValue( values[i] ) ).to.throw( TypeError );
+		}
+		function badValue( value ) {
+			return function() {
+				df.colnames( value );
+			};
+		}
+	});
+
+	it( 'should throw an error if not number of column names does not equal number of columns', function test() {
+		expect( badValue ).to.throw( Error );
+		function badValue() {
+			df.colnames( new Array( data[0].length+1 ) );
+		}
+	});
+
+	it( 'should return an array', function test() {
+		assert.isArray( df.colnames() );
+	});
+
+	it( 'should set the column names', function test() {
+		var expected, actual;
+
+		expected = [ 'beep', 'boop', 'foo' ];
+		df.colnames( expected );
+		actual = df.colnames();
+
+		assert.deepEqual( actual, expected );
+	});
+
+});
---
+++ test/test.rownames.js
@@ -0,0 +1,79 @@
+/* global require, describe, it, beforeEach */
+'use strict';
+
+// MODULES //
+
+var // Expectation library:
+	chai = require( 'chai' ),
+
+	// Module to be tested:
+	DataFrame = require( './../lib' );
+
+
+// VARIABLES //
+
+var expect = chai.expect,
+	assert = chai.assert;
+
+
+// TESTS //
+
+describe( 'rownames', function tests() {
+
+	var data, df;
+
+	beforeEach( function beforeEach() {
+		data = [
+			[1,2,3],
+			[4,5,6]
+		];
+		df = new DataFrame( data );
+	});
+
+	it( 'should provide a method to set/get data frame row names', function test() {
+		expect( df.rownames ).to.be.a( 'function' );
+	});
+
+	it( 'should throw an error if not provided an array', function test() {
+		var values = [
+			'5',
+			5,
+			null,
+			undefined,
+			NaN,
+			function(){},
+			{}
+		];
+
+		for ( var i = 0; i < values.length; i++ ) {
+			expect( badValue( values[i] ) ).to.throw( TypeError );
+		}
+		function badValue( value ) {
+			return function() {
+				df.rownames( value );
+			};
+		}
+	});
+
+	it( 'should throw an error if not number of row names does not equal number of rows', function test() {
+		expect( badValue ).to.throw( Error );
+		function badValue() {
+			df.rownames( new Array( data.length+1 ) );
+		}
+	});
+
+	it( 'should return an array', function test() {
+		assert.isArray( df.rownames() );
+	});
+
+	it( 'should set the row names', function test() {
+		var expected, actual;
+
+		expected = [ 'beep', 'boop' ];
+		df.rownames( expected );
+		actual = df.rownames();
+
+		assert.deepEqual( actual, expected );
+	});
+
+});
